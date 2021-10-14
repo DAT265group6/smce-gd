9468d09e5acc60194fed2584f078c61c7d3b9d68
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-10-14 16:57:22 +0200

    Delete build/

--- build/CMakeFiles/godotcpp-generated.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/godotcpp-generated"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/godotcpp-generated.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
