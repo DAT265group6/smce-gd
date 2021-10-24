b76c75251a0cfbe748d49e1ebbc84920d3c2dbd7
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-10-24 16:03:06 +0200

    Update Entry.gd
    # Check if CMake with the version cmake_version is already downloaded or not
    # Check if CMake with any version exists on the host or not
    # Find current highest CMake version found on host (comparing)
    # Code documentation for functions and variables

--- project/Entry.gd
+++ project/Entry.gd
@@ -105,12 +105,38 @@ func _error(message: String) -> void:
 func _on_clipboard_copy() -> void:
 	OS.clipboard = error
 
-var cmake_version = "cmake-3.21.3-"
+func _get_cmake_folders(path: String):
+	var files = []
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file = dir.get_next()
+		if file != "":
+			if file == "." or file == "..":
+				continue
+			if "cmake" in file:
+				files.append(file)
+		else:
+			break
+	
+	dir.list_dir_end()
+	return files
+
+# Manually update value of this variable to download the CMake version that you wish
+# Ideally pick one version that is higher version than the minimum required version
+var cmake_version = "3.21.3"
+
+# ccrv: Current CMake Required Version
+var ccrv = "3.12"
+
+var cmake_version_name = "cmake-" + cmake_version + "-"
 var cmake_os = ["linux-x86_64", "macos-universal", "windows-x86_64"]
 var osi = {
-	"X11": [cmake_version + cmake_os[0] +".tar.gz", cmake_version + cmake_os[0] +"/bin", "/cmake"],
-	"OSX": [cmake_version + cmake_os[1] + ".tar.gz", cmake_version + cmake_os[1] + "/CMake.app/Contents/bin", "/cmake"],
-	"Windows": [cmake_version + cmake_os[2] + ".zip", cmake_version + cmake_os[2] + "/bin", "/cmake.exe"]
+	"X11": [cmake_version_name + cmake_os[0] +".tar.gz", cmake_version_name + cmake_os[0] +"/bin", "/cmake"],
+	"OSX": [cmake_version_name + cmake_os[1] + ".tar.gz", cmake_version_name + cmake_os[1] + "/CMake.app/Contents/bin", "/cmake"],
+	"Windows": [cmake_version_name + cmake_os[2] + ".zip", cmake_version_name + cmake_os[2] + "/bin", "/cmake.exe"]
 }
 
 func _download_cmake():
@@ -122,17 +148,126 @@ func _download_cmake():
 	var cmake_exec: String = cmake_path + da[1] + da[2]
 	var cmake_ver = []
 
+	# Check if CMake with the version cmake_version is already downloaded or not
 	if File.new().file_exists(cmake_exec):
 		if OS.execute(cmake_exec, ["--version"], true, cmake_ver) != 0:
 			return false
+		print("CMake executable found on host!")
+	
+	# Check if CMake with any version exists on the host or not
+	var cmake_folders = _get_cmake_folders(cmake_path)
+	if cmake_folders.size() > 0:
+		print("Finding highest CMake version found on host...")
+		# cchvd: Current CMake Highest Version Directory (found on host)
+		# E.g. "cmake-3.21.3-windows-x86_64"
+		var cchvd
+
+		# cchvs: Current CMake Highest Version String (found on host)
+		# E.g. "3.21.3"
+		var cchvs
+
+		# ccvs_arr: ccvs array
+		# E.g. [3, 8, 2]
+		# cchvs_arr: cchvs array
+		# E.g. [3, 21, 3]
+		var ccvs_arr
+		var cchvs_arr
+
+		# Index for ccvs_arr/ccrv_arr
+		var i
+
+		# Index for cchvs_arr
+		var j
+
+		# Initialize variables
+		cchvd = cmake_folders[0]
+		cchvs = cchvd.split("-")
+		cchvs = cchvs[1]
+
+		# ccvd: Current CMake Version Directory (found on host)
+		# E.g. "cmake-3.8.2-win64-x64"
+		for ccvd in cmake_folders:
+			# ccvs: Current CMake Version String (found on host)
+			# E.g. "3.8.2"
+			var ccvs
+			ccvs = ccvd.split("-")
+			ccvs = ccvs[1]
+
+			# ccvs_arr: ccvs array
+			# E.g. [3, 8, 2]
+			# cchvs_arr: cchvs array
+			# E.g. [3, 21, 3]
+			ccvs_arr = ccvs.split(".")
+			cchvs_arr = cchvs.split(".")
+			
+			# Making sure both ccvs_arr and cchvs_arr has the same array size
+			# E.g.
+			#	if cchvs_arr is [3, 21] and ccvs_arr is [3, 8, 2] then:
+			# 		append cchvs_arr to make it [3, 21, 0] so both arrays have equal size, vice versa
+			while true:
+				if ccvs_arr.size() > cchvs_arr.size():
+					cchvs_arr.append("0")
+				elif ccvs_arr.size() < cchvs_arr.size():
+					ccvs_arr.append("0")
+				else:
+					break
+			
+			i = 0
+			j = 0
+
+			# Comparing to find CMake Highest Version (found on host)
+			# Updates cchvd and cchvs
+			while true:
+				if i <= ccvs_arr.size() - 1:
+					if int(ccvs_arr[i]) == int(cchvs_arr[j]):
+						i = i + 1
+						j = j + 1
+						continue
+					if int(ccvs_arr[i]) > int(cchvs_arr[j]):
+						cchvd = ccvd
+						cchvs = ccvs
+						break
+					elif int(ccvs_arr[i]) < int(cchvs_arr[j]):
+						break
+				else:
+					break
+
+		ccrv_arr = ccrv.split(".")
+		while true:
+			if ccrv_arr.size() > cchvs_arr.size():
+				cchvs_arr.append("0")
+			elif ccrv_arr.size() < cchvs_arr.size():
+				ccrv_arr.append("0")
+			else:
+				break
 		
-		print("CMake found on host!")
-		
+		print("Checking if highest CMake version (found on host) is more updated version than the minimum required version...")
+		i = 0
+		j = 0
+		while true:
+			if i <= ccrv_arr.size() - 1:
+				if int(ccrv_arr[i]) == int(cchvs_arr[j]):
+					i = i + 1
+					j = j + 1
+					continue
+				if int(ccrv_arr[i]) > int(cchvs_arr[j]):
+					print("Highest CMake version that was found on host is higher version than the minimum required CMake version!")
+					# Todo: Run "cmake --version" for confirmation
+					break
+				elif int(ccrv_arr[i]) < int(cchvs_arr[j]):
+					print("Highest CMake version that was found on host is lower version than the minimum required CMake version!")
+					# Todo: Upgrade cmake version (download)
+					#		Run "cmake --version" for confirmation
+					break
+			else:
+				break
+
+
 	else:
 		if ! File.new().file_exists(cmake_zip):
 			print("Downloading CMake zip...")
 			_request.download_file = cmake_zip + ".download"
-			if ! _request.request("https://github.com/Kitware/CMake/releases/download/v3.21.3/%s" % da[0]):
+			if ! _request.request("https://github.com/Kitware/CMake/releases/download/v" + cmake_version + "/%s" % da[0]):
 				var ret = yield(_request, "request_completed")
 				Directory.new().copy(_request.download_file, cmake_zip)
 				Directory.new().remove(_request.download_file)
@@ -154,6 +289,6 @@ func _download_cmake():
 			return false
 		
 		print("CMake has been downloaded and installed on host!")
-
-	print("--\n%s--" % cmake_ver.front())
+	
+	print("%s" % cmake_ver.front())
 	return cmake_exec
