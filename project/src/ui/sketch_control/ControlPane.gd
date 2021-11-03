86a393be077ebed485d85c5b35116b03cb1ad694
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-11-03 19:30:23 +0100

    Autoinstall cmake (#3)
    * [CMake] Only require Godot before using it
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>
    
    * [*] Update to libSMCE v1.4
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>
    
    * [CI] Parameterize on libSMCE version & tag
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>
    
    * cmake: track godot-cpp 3.x branch"
    
    master started development for godot 4.0
    
    * bind: add missing headers
    
    * [CMake+CI] Sync packaging to libSMCE level; skip RPMs
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>
    
    * Vehicle pos (#1)
    
    * added the functionality of changing position
    
    * completed the starter task, co-work with benik
    
    * Modified Entry.gd & Entry.tscn & Util.gd
    
    Implemented Download & Autoinstall CMake (version check not yet implemented)
    
    Co-Authored-By: Clementine Jensen <gusjensecl@student.gu.se>
    
    * Update .gitignore
    
    * Delete build/
    
    * Fix weird resource missing error
    
    * Reformat Entry.gd
    
    Reduce duplication
    
    * Reformat Entry.gd (again)
    
    Reduce code duplication
    
    * Update Entry.gd
    
    # Check if CMake with the version cmake_version is already downloaded or not
    # Check if CMake with any version exists on the host or not
    # Find current highest CMake version found on host (comparing)
    # Code documentation for functions and variables
    
    * Fix missing resource
    
    * Documentation
    
    * Update Entry.gd
    
    # Search CMake execution path
    # Search CMake download url
    
    * Update .gitignore
    
    * res
    
    * mby fix error
    
    * Vehicle pos (#1)
    
    * added the functionality of changing position
    
    * completed the starter task, co-work with benik
    
    * Modified Entry.gd & Entry.tscn & Util.gd
    
    Implemented Download & Autoinstall CMake (version check not yet implemented)
    
    Co-Authored-By: Clementine Jensen <gusjensecl@student.gu.se>
    
    * Update .gitignore
    
    * Delete build/
    
    * Fix weird resource missing error
    
    * Reformat Entry.gd
    
    Reduce duplication
    
    * Reformat Entry.gd (again)
    
    Reduce code duplication
    
    * Update Entry.gd
    
    # Check if CMake with the version cmake_version is already downloaded or not
    # Check if CMake with any version exists on the host or not
    # Find current highest CMake version found on host (comparing)
    # Code documentation for functions and variables
    
    * Fix missing resource
    
    * Documentation
    
    * Update Entry.gd
    
    # Search CMake execution path
    # Search CMake download url
    
    * Update .gitignore
    
    * res
    
    * mby fix error
    
    * Rebase to master
    
    Co-authored-by: AeroStun <24841307+AeroStun@users.noreply.github.com>
    Co-authored-by: Ruthger Dijt <ruthgerdijt@gmail.com>
    Co-authored-by: Wei Guo <37690405+Weiguo98@users.noreply.github.com>
    Co-authored-by: Clementine Jensen <gusjensecl@student.gu.se>

--- project/src/ui/sketch_control/ControlPane.gd
+++ project/src/ui/sketch_control/ControlPane.gd
@@ -377,7 +377,7 @@ func reset_vehicle_pos() -> void:
 		return
 	var was_frozen = vehicle.frozen
 	vehicle.freeze()
-	vehicle.global_transform.origin = Vector3(20,3,0)
+	vehicle.global_transform.origin = init_vec_pos()
 	vehicle.global_transform.basis = Basis()
 	if ! was_frozen:
 		vehicle.unfreeze()
