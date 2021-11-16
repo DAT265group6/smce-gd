35dcdbb8cb07a5ad29cab071d3a689a274ae87c4
Elena <elena@manele.se>
2021-11-16 20:28:33 +0100

    Small changes before demonstration

--- CMake/Modules/SetupSMCE.cmake
+++ CMake/Modules/SetupSMCE.cmake
@@ -1,5 +1,5 @@
 #
-#  UserConfigVars.cmake
+#  SetupSMCE.cmake
 #  Copyright 2021 ItJustWorksTM
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,9 +17,9 @@
 
 include_guard ()
 
-#find_package (SMCE 1.4 REQUIRED)
-if(TRUE)
-    message("libsmce are not int your computer, autoinstalling")
+find_package (SMCE 1.4)
+if(NOT SMCE_FOUND)
+    message("libSMCE not found, downloading and installing...")
 
     # set the libSMCE version and the target file basename
     set (SMCE_EXPECTED_TAG 1.4.0)
@@ -34,7 +34,6 @@ if(TRUE)
     set (SMCE_ARK_FILENAME "${SMCE_BASENAME}.zip")
     set (SMCE_ROOT "${CMAKE_CURRENT_BINARY_DIR}/smce-autodl")
     file (MAKE_DIRECTORY "${SMCE_ROOT}")
-#DEBUG    message("libSMCE-${SMCE_EXPECTED_VERSION}-${CMAKE_SYSTEM_NAME}-${SMCE_EXPECTED_ARCH}-${CMAKE_CXX_COMPILER_ID}")
 
     # download sha512.txt from github
     file (DOWNLOAD "https://github.com/ItJustWorksTM/libSMCE/releases/download/v${SMCE_EXPECTED_TAG}/sha512.txt"
@@ -46,13 +45,12 @@ if(TRUE)
         string (SUBSTRING "${SMCE_SHA512_LINE}" 130 -1 SMCE_SHA512_FNAME)
         if (SMCE_SHA512_FNAME STREQUAL SMCE_ARK_FILENAME)
             string (SUBSTRING "${SMCE_SHA512_LINE}" 0 128 SMCE_ARK_HASH)
-            message(${SMCE_SHA512_LINE})
                 break ()
         endif ()
     endforeach ()
 
-    # downlaod the zip file from github and make it in the corresponding dir
-   file (DOWNLOAD "https://github.com/ItJustWorksTM/libSMCE/releases/download/v${SMCE_EXPECTED_TAG}/${SMCE_ARK_FILENAME}"
+    # download the zip file from github and make it in the corresponding dir
+    file (DOWNLOAD "https://github.com/ItJustWorksTM/libSMCE/releases/download/v${SMCE_EXPECTED_TAG}/${SMCE_ARK_FILENAME}"
             "${SMCE_ROOT}/${SMCE_ARK_FILENAME}"
             SHOW_PROGRESS
             TLS_VERIFY ON
@@ -61,20 +59,17 @@ if(TRUE)
             WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
     file (REMOVE_RECURSE "${SMCE_ROOT}")
 
-
     add_library (SMCE IMPORTED STATIC)
     target_include_directories (SMCE INTERFACE "${SMCE_ROOT}/include")
-    message(${CMAKE_STATIC_LIBRARY_SUFFIX})
 
     # make the target to the libSMCE_static.a
     set_property (TARGET SMCE PROPERTY IMPORTED_LOCATION "${SMCE_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}SMCE_static${CMAKE_STATIC_LIBRARY_SUFFIX}")
     set(SMCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/${SMCE_BASENAME}/lib/cmake/SMCE")
+    message ("libSMCE installed in ${CMAKE_CURRENT_BINARY_DIR}/${SMCE_BASENAME}")
+    find_package (SMCE 1.4 REQUIRED)
 endif()
 
 # refind the package and output the dir to make sure it used the download one
-find_package (SMCE 1.4 REQUIRED)
-message("${SMCE_DIR}")
-message ("libSMCE found")
 set (SMCE_TARGET SMCE::SMCE)
 option (SMCE_LINK_STATIC "Link to libSMCE statically" Off)
 
@@ -90,60 +85,3 @@ if (APPLE)
     set (CMAKE_BUILD_RPATH "@loader_path/../Frameworks;${CMAKE_BUILD_RPATH}")
     set (CMAKE_INSTALL_RPATH "@loader_path/../Frameworks;${CMAKE_INSTALL_RPATH}")
 endif ()
-
-#    file (GLOB WA_BOOST_LIBS LIST_DIRECTORIES false "${SMCE_ROOT}/lib64/boost/*")
-#    message ("Found the following Boost workaround libs: ${WA_BOOST_LIBS}")
-#    if (WA_BOOST_LIBS)
-#        add_library (WA_Boost INTERFACE)
-#        foreach (WA_BLIB ${WA_BOOST_LIBS})
-#            if (IS_SYMLINK "${WA_BOOST_LIBS}")
-#                message (FATAL_ERROR "Workaround Boost lib \"${WA_BLIB}\" is a symlink")
-#            elseif (IS_DIRECTORY "${WA_BOOST_LIBS}")
-#                message (FATAL_ERROR "Workaround Boost lib \"${WA_BLIB}\" is a directory")
-#            endif ()
-#            target_link_libraries (WA_Boost INTERFACE "${WA_BLIB}")
-#        endforeach ()
-#        target_link_libraries (SMCE INTERFACE WA_Boost)
-#    endif ()
-#endif()
-
-#include (FetchContent)
-#message ("--- Downloading libsmce")
-#FetchContent_Declare (libsmce
-#        GIT_REPOSITORY "https://github.com/ItJustWorksTM/libSMCE"
-#        GIT_TAG v1.4.0
-#        GIT_SHALLOW On)
-#FetchContent_GetProperties (libsmce)
-#if (NOT libsmce_POPULATED)
-#    FetchContent_Populate (libsmce)
-#    message ("--- populated libsmce")
-#    file (READ "${libsmce_SOURCE_DIR}/CMakeLists.txt" libsmce_cmakelists)
-#    string (REPLACE "add_dependencies (SMCE ArdRtRes)" "add_dependencies (SMCE_static ArdRtRes)" libsmce_cmakelists "${libsmce_cmakelists}")
-#    file (WRITE "${libsmce_SOURCE_DIR}/CMakeLists.txt" "${libsmce_cmakelists}")
-#
-#    set (SMCE_BUILD_SHARED Off CACHE INTERNAL "")
-#    set (SMCE_BUILD_STATIC On CACHE INTERNAL "")
-#    set (SMCE_CXXRT_LINKING "STATIC" CACHE INTERNAL "")
-#    set (SMCE_BOOST_LINKING "${PYSMCE_BOOST_LINKING}" CACHE INTERNAL "")
-#    add_subdirectory ("${libsmce_SOURCE_DIR}" "${libsmce_BINARY_DIR}" EXCLUDE_FROM_ALL)
-#endif ()
-
-
-#find_package (SMCE 1.4 REQUIRED)
-#add_library(SMCE 1.4 INTERFACE)
-#add_library (SMCE_Boost INTERFACE)
-#if ("${SMCE_BOOST_LINKING}" STREQUAL "STATIC")
-#    set (Boost_USE_STATIC_LIBS True)
-#else ()
-#    set (Boost_USE_STATIC_LIBS False)
-#endif ()
-
-#if (MSVC)
-#    find_package (Boost 1.74 COMPONENTS atomic filesystem date_time)
-#else ()
-#    find_package (Boost 1.74 COMPONENTS atomic filesystem)
-#endif ()
-
-#if(NOT SMCE_ROOT)
-#
-#endif()
